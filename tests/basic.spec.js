4161ea7637b87d471e58e6c103bc67139e382f49
Kai Lepler <15275432+KaiLepler@users.noreply.github.com>
2025-11-22 15:36:55 +0100

    test: adjust Playwright navigation paths from absolute to relative.

--- tests/basic.spec.js
+++ tests/basic.spec.js
@@ -3,28 +3,28 @@ const { test, expect } = require('@playwright/test');
 
 test.describe('Eleventy Site Tests', () => {
   test('Homepage Test', async ({ page }) => {
-    await page.goto('/');
+    await page.goto('./');
     await expect(page).toHaveTitle(/Welcome to My Personal Homepage/);
     await expect(page.getByRole('heading', { name: 'Welcome to My Personal Homepage!', level: 1 })).toBeVisible();
   });
 
   test.describe('Navigation Tests', () => {
     test('Navigate to About Me page', async ({ page }) => {
-      await page.goto('/');
+      await page.goto('./');
       await page.getByRole('link', { name: 'About Me' }).click();
       await expect(page).toHaveURL(/.*\/about\//);
       await expect(page.getByRole('heading', { name: 'About Me', level: 1 })).toBeVisible();
     });
 
     test('Navigate to Blog page', async ({ page }) => {
-      await page.goto('/');
+      await page.goto('./');
       await page.getByRole('navigation').getByRole('link', { name: 'Blog' }).click();
       await expect(page).toHaveURL(/.*\/blog\//);
       await expect(page.getByRole('heading', { name: 'My Blog', level: 1 })).toBeVisible();
     });
 
     test('Navigate to Photography page', async ({ page }) => {
-      await page.goto('/');
+      await page.goto('./');
       await page.getByRole('link', { name: 'Photography' }).click();
       await expect(page).toHaveURL(/.*\/photography\//);
       await expect(page.getByRole('heading', { name: 'Photography', level: 1 })).toBeVisible();
@@ -33,7 +33,8 @@ test.describe('Eleventy Site Tests', () => {
 
   test.describe('Blog Functionality Tests', () => {
     test('Published and draft posts on Blog page', async ({ page }) => {
-      await page.goto('/blog/');
+      await page.goto('blog/');
+      console.log('Current URL:', page.url());
       // Check for the published post
       await expect(page.getByRole('link', { name: 'My First Blog Post' })).toBeVisible();
       // Check that the draft post is NOT visible
@@ -43,7 +44,7 @@ test.describe('Eleventy Site Tests', () => {
 
   test.describe('Admin Page - Drafts Test', () => {
     test('Drafts visibility on Admin page', async ({ page }) => {
-      await page.goto('/admin/');
+      await page.goto('admin/');
       await expect(page.getByRole('heading', { name: 'Blog Post Manager', level: 1 })).toBeVisible();
 
       // Check that the draft post IS visible
